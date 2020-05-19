88462d5332d5cb86228dce37a44acdd0dd664f66
K7U1U <65617431+K7U1U@users.noreply.github.com>
2020-05-19 19:31:15 +0200

    Update wfw.sh

--- wfw.sh
+++ wfw.sh
@@ -9,18 +9,18 @@ else
     exit  
 fi
   
-echo -e "\033[1mTotal Access Points: \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`\e[0m"
-echo -e "\033[1mTotal Stations: \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`\e[0m"
-echo -e "\033[1mTotal Stations without association: \033[0;31m`grep -E '(not associated)' $FILE | wc -l`\e[0m"
+echo -e "\033[1mTotal Points d'Accès : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`\e[0m"
+echo -e "\033[1mTotal Stations : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`\e[0m"
+echo -e "\033[1mTotal Stations sans association : \033[0;31m`grep -E '(not associated)' $FILE | wc -l`\e[0m"
   
-echo -e "\033[0;36m\033[1mAvailable Access Points:\e[0m"
+echo -e "\033[0;36m\033[1mPoints d'Accès disponibles :\e[0m"
   
 while read -r line ; do
   
     if [ "`echo "$line" | cut -d ',' -f 14`" != " " ]; then
         echo -e "\033[1m" `echo -e "$line" | cut -d ',' -f 14` "\e[0m"
     else
-        echo -e " \e[3mcould not get network name (ESSID)\e[0m"
+        echo -e " \e[3mNe peut pas déterminer nom du réseau (ESSID)\e[0m"
     fi
   
     fullMAC=`echo "$line" | cut -d ',' -f 1`
@@ -33,13 +33,13 @@ while read -r line ; do
     if [ "$result" ]; then
         echo -e "\tVendor: `echo "$result" | cut -f 3`"
     else
-        echo -e "\tVendor: \e[3mInformation not found in the database.\e[0m"
+        echo -e "\tVendor: \e[3mInformation non trouvée dans la base de données.\e[0m"
     fi
   
     is5ghz=`echo "$line" | cut -d ',' -f 4 | grep -i -E '36|40|44|48|52|56|60|64|100|104|108|112|116|120|124|128|132|136|140'`
   
     if [ "$is5ghz" ]; then
-        echo -e "\t\033[0;31mIt operates at 5 GHz!\e[0m"
+        echo -e "\t\033[0;31mOpère à 5 GHz!\e[0m"
     fi
   
     printonce="\tStations:"
@@ -64,28 +64,28 @@ while read -r line ; do
                 ismobile=`echo $result2 | grep -i -E 'Olivetti|Sony|Mobile|Apple|Samsung|HUAWEI|Motorola|TCT|LG|Ragentek|Lenovo|Shenzhen|Intel|Xiaomi|zte'`
                 warning=`echo $result2 | grep -i -E 'ALFA|Intel'`
                 if [ "$ismobile" ]; then
-                    echo -e "\t\t\t\033[0;33mThis is probably a mobile device\e[0m"
+                    echo -e "\t\t\t\033[0;33mC'est possiblement un mobile\e[0m"
                 fi
   
                 if [ "$warning" ]; then
-                    echo -e "\t\t\t\033[0;31;5;7mThe device can support the monitor mode\e[0m"
+                    echo -e "\t\t\t\033[0;31;5;7mLe périphérique supporte le mode monitor\e[0m"
                 fi
   
             else
-                echo -e "\t\t\tVendor: \e[3mInformation not found in the database.\e[0m"
+                echo -e "\t\t\tVendor: \e[3mInformation non trouvée dans la base de données.\e[0m"
             fi
   
             probed=`echo $line2 | cut -d ',' -f 7`
   
             if [ "`echo $probed | grep -E [A-Za-z0-9_\\-]+`" ]; then
-                echo -e "\t\t\tIt probed networks: $probed"
+                echo -e "\t\t\tA recherché le réseau : $probed"
             fi        
         fi
     done < <(grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE)
       
 done < <(grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE)
   
-echo -e "\033[0;36m\033[1mNot associated Stations:\e[0m"
+echo -e "\033[0;36m\033[1mStations non associées :\e[0m"
   
 while read -r line2 ; do
   
@@ -101,19 +101,19 @@ while read -r line2 ; do
         ismobile=`echo $result2 | grep -i -E 'Olivetti|Sony|Mobile|Apple|Samsung|HUAWEI|Motorola|TCT|LG|Ragentek|Lenovo|Shenzhen|Intel|Xiaomi|zte'`
         warning=`echo $result2 | grep -i -E 'ALFA|Intel'`
         if [ "$ismobile" ]; then
-            echo -e "\t\033[0;33mThis is probably a mobile device\e[0m"
+            echo -e "\t\033[0;33mC'est possiblement un mobile\e[0m"
         fi
         if [ "$warning" ]; then
-            echo -e "\t\033[0;31;5;7mThe device can support the monitor mode\e[0m"
+            echo -e "\t\033[0;31;5;7mCe périphérique supporte le mode monitor\e[0m"
         fi
     else
-        echo -e "\tVendor: \e[3mInformation not found in the database.\e[0m"
+        echo -e "\tVendor: \e[3mInformations non trouvées dans la base de données.\e[0m"
     fi
   
     probed=`echo $line2 | cut -d ',' -f 7`
   
     if [ "`echo $probed | grep -E [A-Za-z0-9_\\-]+`" ]; then
-        echo -e "\tIt probed networks: $probed"
+        echo -e "\tA recherché le réseau : $probed"
     fi        
   
 done < <(grep -E '(not associated)' $FILE)
