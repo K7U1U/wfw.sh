ff4f0a0691ce6f7582a0a235b61caa3560422285
K7U1U <65617431+K7U1U@users.noreply.github.com>
2020-05-19 21:06:48 +0200

    Update wfw.sh

--- wfw.sh
+++ wfw.sh
@@ -3,15 +3,15 @@
 if [[ "$1" && -f "$1" ]]; then
     FILE="$1"
 else
-    echo 'Specify the .csv file to analyze.';
+    echo 'Indiqer le fichier .csv à parser.';
     echo 'Usage:';
     echo -e "\tbash wfw.sh /tmp/test-01.csv";
     exit  
 fi
   
-echo -e "\033[1mTotal Points d'Accès : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`\e[0m"
-echo -e "\033[1mTotal Stations : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`\e[0m"
-echo -e "\033[1mTotal Stations sans association : \033[0;31m`grep -E '(not associated)' $FILE | wc -l`\e[0m"
+echo -e "\033[1mPoints d'Accès : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`\e[0m"
+echo -e "\033[1mStations : \033[0;31m`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`\e[0m"
+echo -e "\033[1mStations sans association : \033[0;31m`grep -E '(not associated)' $FILE | wc -l`\e[0m"
   
 echo -e "\033[0;36m\033[1mPoints d'Accès disponibles :\e[0m"
   
@@ -20,7 +20,7 @@ while read -r line ; do
     if [ "`echo "$line" | cut -d ',' -f 14`" != " " ]; then
         echo -e "\033[1m" `echo -e "$line" | cut -d ',' -f 14` "\e[0m"
     else
-        echo -e " \e[3mNe peut pas déterminer nom du réseau (ESSID)\e[0m"
+        echo -e " \e[3mNe peut pas déterminer le nom du réseau (ESSID)\e[0m"
     fi
   
     fullMAC=`echo "$line" | cut -d ',' -f 1`
@@ -64,7 +64,7 @@ while read -r line ; do
                 ismobile=`echo $result2 | grep -i -E 'Olivetti|Sony|Mobile|Apple|Samsung|HUAWEI|Motorola|TCT|LG|Ragentek|Lenovo|Shenzhen|Intel|Xiaomi|zte'`
                 warning=`echo $result2 | grep -i -E 'ALFA|Intel'`
                 if [ "$ismobile" ]; then
-                    echo -e "\t\t\t\033[0;33mC'est possiblement un mobile\e[0m"
+                    echo -e "\t\t\t\033[0;33mIl s'agit possiblement un mobile\e[0m"
                 fi
   
                 if [ "$warning" ]; then
@@ -101,7 +101,7 @@ while read -r line2 ; do
         ismobile=`echo $result2 | grep -i -E 'Olivetti|Sony|Mobile|Apple|Samsung|HUAWEI|Motorola|TCT|LG|Ragentek|Lenovo|Shenzhen|Intel|Xiaomi|zte'`
         warning=`echo $result2 | grep -i -E 'ALFA|Intel'`
         if [ "$ismobile" ]; then
-            echo -e "\t\033[0;33mC'est possiblement un mobile\e[0m"
+            echo -e "\t\033[0;33mIl s'agit possiblement un mobile\e[0m"
         fi
         if [ "$warning" ]; then
             echo -e "\t\033[0;31;5;7mCe périphérique supporte le mode monitor\e[0m"
